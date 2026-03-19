735435768b29da202e13df72322c5eadc7aade45
arturavn <arturavn@github.com>
2026-03-19 15:48:17 -0300

    fix: Express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use('/api', router)
 // Serve built frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')))
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')))
+  app.get('/{*path}', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')))
 }
 
 initDb().then(() => {
